120f29207a8b90ae8a18acbad9510742b2a75cbb
aravi <aravi@ARAVINDH>
2024-09-06 11:50:13 +0530

    This is my first Commit

---
+++ C2TC_Aravindh/src/com/tns/Day1/FirstProgram.java
@@ -0,0 +1,38 @@
+package com.tns.Day1;
+
+public class FirstProgram {
+
+		    public static void main(String[] args) {
+		        int num1 = 10;
+		        int num2 = 5;
+
+		        // Addition
+		        int sum = num1 + num2;
+		        System.out.println("Sum: " + sum);
+
+		        // Subtraction
+		        int difference = num1 - num2;
+		        System.out.println("Difference: " + difference);
+
+		        // Multiplication
+		        int product = num1 * num2;
+		        System.out.println("Product: " + product);
+
+		        // Division
+		        int quotient = num1 / num2;
+		        System.out.println("Quotient: " + quotient);
+
+		        // Modulus (remainder)
+		        int remainder = num1 % num2;
+		        System.out.println("Remainder: " + remainder);
+
+		        // Increment
+		        num1++;
+		        System.out.println("Incremented num1: " + num1);
+
+		        // Decrement
+		        num2--;
+		        System.out.println("Decremented num2: " + num2);
+		    }
+		}
+
